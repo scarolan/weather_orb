9809d0080c89571bdc2a015540b0c7c0673f42c2
Unix Minion <scarolan@gmail.com>
2013-05-25 13:32:53 -0500

    Create orb_listener.py
    This was the easiest way I could come up with to have something listening for input, without complicating the main program that actually runs the various functions.

---
+++ orb_listener.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+# Listens for mode button input.  For use with orb.py
+
+import RPi.GPIO as gpio
+import os
+from time import sleep
+MODES = ('weather','moodlight','alarmclock')
+
+# Set up our mode button pin for input
+MODEPIN = 10
+gpio.setwarnings(False)
+gpio.setmode(gpio.BCM)
+gpio.setup(MODEPIN, gpio.IN)
+
+# Command for killing the current orb.py process
+killcmd="kill -9 $(ps auxwww | grep orb.py | head -1 | awk '{ print $2 }')"
+orbscript='/usr/local/bin/orb.py'
+
+currmode = 2
+while True:
+    if gpio.input(MODEPIN) == False:
+        if currmode < len(MODES) - 1:
+            currmode += 1
+        else:
+            currmode = 0
+        print MODES[currmode]
+        os.system(killcmd)
+        os.system(orbscript+' '+MODES[currmode]+' &')
+        print orbscript+' '+MODES[currmode]
+    sleep(0.5)
